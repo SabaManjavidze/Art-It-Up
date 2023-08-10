ef6f94671cfe4f8e2155df2e01c06c1ca7d85e3d
SabaManjavidze <sabamanjavidze@gmail.com>
2023-08-11 02:16:45 +0400

    added url change loader

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Footer } from "./Footer";
+import { BarLoader } from "react-spinners";
+import { Router } from "next/router";
 
 export default function Layout({
   title = "Art It Up",
@@ -9,6 +11,20 @@ export default function Layout({
   title?: string;
   children: React.ReactElement | React.ReactElement[] | null;
 }) {
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    Router.events.on("routeChangeStart", (url) => {
+      setIsLoading(true);
+    });
+
+    Router.events.on("routeChangeComplete", (url) => {
+      setIsLoading(false);
+    });
+
+    Router.events.on("routeChangeError", (url) => {
+      setIsLoading(false);
+    });
+  }, [Router]);
   return (
     <>
       <Head>
@@ -17,6 +33,11 @@ export default function Layout({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between bg-background text-primary-foreground">
+        <BarLoader
+          loading={isLoading}
+          className="!fixed z-50 !w-full !bg-cyan-500"
+          height={6}
+        />
         <main>{children}</main>
         <Footer />
       </div>
